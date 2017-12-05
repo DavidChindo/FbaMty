9fd87b9154007ae29c460fe98c049e56e5f8c448
DavidChindo <dbarrera.chindo@gmail.com>
2017-12-05 17:18:36 -0600

    init layout pre-menu

--- app/src/main/java/com/fibramty/fbmty/View/Activity/MainActivity.java
+++ app/src/main/java/com/fibramty/fbmty/View/Activity/MainActivity.java
@@ -2,17 +2,22 @@ package com.fibramty.fbmty.View.Activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.fibramty.fbmty.R;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.toolbar)Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+        toolbar.setTitle("Servicios");
     }
 }
